b14e55c3a3b04b78d6f8588a3e87a5ba00504c87
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-03-24 18:33:11 +0530

    Refactor Header component for improved readability

--- SettyRents-master/src/components/Header/index.js
+++ SettyRents-master/src/components/Header/index.js
@@ -2,22 +2,17 @@ import {FaBars} from 'react-icons/fa'
 import './index.css'
 
 const Header = ({toggleSidebar}) => {
-return ( <div className="header"> <div className="header-left"> <button
-       className="menu-btn"
-       onClick={toggleSidebar}
-     > <FaBars /> </button>
-
-
-    <h1>Rental Management System</h1>
-  </div>
-
-  <button className="logout-btn">
-    Logout
-  </button>
-</div>
-
-
-)
+  return (
+    <div className='header'>
+      <div className='header-left'>
+        <button className='menu-btn' onClick={toggleSidebar}>
+          <FaBars />
+        </button>
+        <h1>Rental Management System</h1>
+      </div>
+      <button className='logout-btn'>Logout</button>
+    </div>
+  )
 }
 
 export default Header
