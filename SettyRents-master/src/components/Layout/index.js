d3650c4d5cb1563de8360bf139fbb0aea47a47db
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-03-24 18:32:00 +0530

    Refactor Layout component structure and comments

--- SettyRents-master/src/components/Layout/index.js
+++ SettyRents-master/src/components/Layout/index.js
@@ -6,22 +6,17 @@ import './index.css'
 const Layout = ({children}) => {
   const [isOpen, setIsOpen] = useState(false)
 
-  // Toggle sidebar open/close
   const toggleSidebar = () => setIsOpen(!isOpen)
-
-  // Close sidebar (used when clicking a menu item or overlay)
   const closeSidebar = () => setIsOpen(false)
 
   return (
     <div className='layout'>
-      {/* Sidebar with isOpen state */}
+      {/* Sidebar */}
       <Sidebar isOpen={isOpen} closeSidebar={closeSidebar} />
 
+      {/* Main content */}
       <div className='main-container'>
-        {/* Header with hamburger toggle */}
         <Header toggleSidebar={toggleSidebar} />
-
-        {/* Main content */}
         <div className='content'>{children}</div>
       </div>
     </div>
