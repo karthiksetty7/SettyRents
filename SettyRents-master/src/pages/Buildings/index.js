b7f5a04cbc87c26c3615219a105244a7ba4c4717
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-03-25 18:47:30 +0530

    Update index.js

--- SettyRents-master/src/pages/Buildings/index.js
+++ SettyRents-master/src/pages/Buildings/index.js
@@ -3,76 +3,75 @@ import Layout from '../../components/Layout'
 import './index.css'
 
 const Buildings = () => {
-const [buildingName, setBuildingName] = useState('')
-const [address, setAddress] = useState('')
-const [buildings, setBuildings] = useState([])
+  const [buildingName, setBuildingName] = useState('')
+  const [address, setAddress] = useState('')
+  const [buildings, setBuildings] = useState([])
 
-const handleSubmit = e => {
-e.preventDefault()
+  const handleSubmit = e => {
+    e.preventDefault()
 
+    const newBuilding = {
+      id: Date.now(),
+      buildingName,
+      address,
+    }
 
-const newBuilding = {
-  id: Date.now(),
-  buildingName,
-  address,
-}
+    setBuildings([...buildings, newBuilding])
+    setBuildingName('')
+    setAddress('')
+  }
 
-setBuildings([...buildings, newBuilding])
-setBuildingName('')
-setAddress('')
+  return (
+    <Layout>
+      <div className='building-container'>
+        <h2>Add Building</h2>
+        <form className='building-form' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='Building Name'
+            value={buildingName}
+            onChange={e => setBuildingName(e.target.value)}
+            required
+          />
 
+          <input
+            type='text'
+            placeholder='Address'
+            value={address}
+            onChange={e => setAddress(e.target.value)}
+            required
+          />
 
-}
+          <button type='submit'>Add Building</button>
+        </form>
 
-return ( <Layout> <div className="building-container"> <h2>Add Building</h2>
+        <h2>Buildings List</h2>
+        <div className='table-container'>
+          <table>
+            <thead>
+              <tr>
+                <th>Building Name</th>
+                <th>Address</th>
+              </tr>
+            </thead>
 
-
-    <form className="building-form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Building Name"
-        value={buildingName}
-        onChange={e => setBuildingName(e.target.value)}
-        required
-      />
-
-      <input
-        type="text"
-        placeholder="Address"
-        value={address}
-        onChange={e => setAddress(e.target.value)}
-        required
-      />
-
-      <button type="submit">Add Building</button>
-    </form>
-
-    <h2>Buildings List</h2>
-
-    <div className="table-container">
-      <table>
-        <thead>
-          <tr>
-            <th>Building Name</th>
-            <th>Address</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {buildings.map(item => (
-            <tr key={item.id}>
-              <td>{item.buildingName}</td>
-              <td>{item.address}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  </div>
-</Layout>
-
-
-)
+            <tbody>
+              {buildings.map(item => (
+                <tr key={item.id}>
+                  <td data-label='Building Name'>
+                    <span>{item.buildingName}</span>
+                  </td>
+                  <td data-label='Address'>
+                    <span>{item.address}</span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default Buildings
