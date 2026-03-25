bfb3a20982db53c0129fd556615f51bb4936f627
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-03-25 17:55:50 +0530

    Update index.js

--- SettyRents-master/src/components/DashboardCards/index.js
+++ SettyRents-master/src/components/DashboardCards/index.js
@@ -1,18 +1,24 @@
 import './index.css'
 
 const DashboardCards = () => {
-// temporary static data (later from API)
-const data = [
-{title: 'Buildings', count: 5},
-{title: 'Floors', count: 12},
-{title: 'Rooms', count: 48},
-{title: 'Tenants', count: 40},
-]
+  // temporary static data (later from API)
+  const data = [
+    {title: 'Buildings', count: 5},
+    {title: 'Floors', count: 12},
+    {title: 'Rooms', count: 48},
+    {title: 'Tenants', count: 40},
+  ]
 
-return ( <div className="cards-container">
-{data.map(item => ( <div className="card" key={item.title}> <h3>{item.title}</h3> <p>{item.count}</p> </div>
-))} </div>
-)
+  return (
+    <div className='cards-container'>
+      {data.map(item => (
+        <div className='card' key={item.title}>
+          <h3>{item.title}</h3>
+          <p>{item.count}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default DashboardCards
