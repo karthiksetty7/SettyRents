7ba264f659805b525081011014c5e5e63f88f7b4
karthiksetty7 <137997670+karthiksetty7@users.noreply.github.com>
2026-03-25 17:54:34 +0530

    Update index.js

--- SettyRents-master/src/pages/Dashboard/index.js
+++ SettyRents-master/src/pages/Dashboard/index.js
@@ -1,9 +1,22 @@
 import Layout from '../../components/Layout'
 import DashboardCards from '../../components/DashboardCards'
+import './index.css'
 
 const Dashboard = () => {
-return ( <Layout> <h1>Dashboard</h1> <DashboardCards /> </Layout>
-)
+  return (
+    <Layout>
+      {/* Dashboard Header */}
+      <div className='dashboard-header'>
+        <div>
+          <h1>Dashboard</h1>
+          <p className='dashboard-subtitle'>Overview of your recent activity</p>
+        </div>
+      </div>
+
+      {/* Dashboard Cards */}
+      <DashboardCards />
+    </Layout>
+  )
 }
 
 export default Dashboard
